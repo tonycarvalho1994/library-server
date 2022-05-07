283696de3552f6e87c4bba6569c7d879e1820398
Tony Jedson Beserra Carvalho <tony.carvalho1994@gmail.com>
2022-05-07 12:12:26 -0300

    Fixing DATABASE_URL to watch to env vars

--- app/services/database/__init__.py
+++ app/services/database/__init__.py
@@ -1,10 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
 from app.instance.config import ConfigSettings
 
-DATABASE_URL = ConfigSettings.DATABASE_URL
+LOCAL_DATABASE_URL = ConfigSettings.DATABASE_URL
+DATABASE_URL = os.environ.get('DATABASE_URL', LOCAL_DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
